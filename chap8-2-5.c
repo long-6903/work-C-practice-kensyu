07aa7d00537b560bacc68e09bc8a2deb25b4a96d
long6903 <xacnhantklol@gmail.com>
2025-05-26 11:41:15 +0900

    note

--- chap8-2-5.c
+++ chap8-2-5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    char sbinary[17]; 
+    char sbinary[17]; // '\o' is a null terminator
     //17 bit = 16bit + '\o'(in order to be able to print the string out as %s, '\o' must be declared)
     int wattaValue, value, i;
 
@@ -16,10 +16,12 @@ int main(){
         sbinary[i] = wattaValue + '0'; /*turn int type number(wattaValue) to char type character(sbinary[i])*/
     }
     
-    //sbinary[16] = '\0'; // 文字列終端, 直接にfor loopのlimitにしてる
+    sbinary[16] = '\0'; // 文字列終端, 直接にfor loopのlimitにしてる
         // 文字列として表示するには、最後に '\0' を追加する必要があります。
+        // safe: 17th slot used for string terminator
 
     for (int i = 0; sbinary[i] != '\0'; i++) // printing out char by char of sbinary string for testing
+    //Must set sbinary[16] = '\0'; somewhere before this loop, or else this will go out of bounds and potentially crash
     {
         printf("%c ", sbinary[i]);
     }
